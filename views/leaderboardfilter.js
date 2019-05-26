7c90fabbd0b964650a0103f8901931296743a878
jamesmgittins <jamesmgittins@gmail.com>
2019-05-26 23:21:25 +0100

    Added filter for dead characters

--- views/leaderboardfilter.js
+++ views/leaderboardfilter.js
@@ -1,14 +1,17 @@
 var filters = document.getElementById('leaderboard').getElementsByTagName('a');
+var deadFiltered = false;
 
 function allRows(hide) {
     var rows = document.getElementById('leaderboard').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
     for (var i = 0; i < rows.length; i++) {
-        rows[i].style.display = hide ? 'none' : '';
+        rows[i].style.display = hide || (deadFiltered && rows[i].className.indexOf('dead') != -1) ? 'none' : '';
     }
 }
 
 function showRowsForClass(classname) {
     var rows = document.getElementById('leaderboard').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    if (deadFiltered)
+        classname = classname + " alive";
     for (var i = 0; i < rows.length; i++) {
         if(rows[i].className.indexOf(classname) != -1)
             rows[i].style.display = '';
@@ -24,15 +27,24 @@ function highlightButton(element) {
 
 for (var i = 0; i < filters.length; i++) {
     filters[i].addEventListener('click',function(){
-        highlightButton(this);
         allRows(true);
         switch(this.getAttribute('id')) {
             case 'all-filter':
-            allRows(false);
-            break;
+                allRows(false);
+                highlightButton(this);
+                break;
+            case 'dead-filter':
+                deadFiltered = !deadFiltered;
+                if (deadFiltered)
+                    this.innerText = "Show Dead Characters"
+                else
+                    this.innerText = "Hide Dead Characters"
+                allRows(false);
+                break;
             default:
-            showRowsForClass(this.getAttribute('id').substring(0,this.getAttribute('id').indexOf('-')));
-            break;
+                showRowsForClass(this.getAttribute('id').substring(0,this.getAttribute('id').indexOf('-')));
+                highlightButton(this);
+                break;
         }
     });
 }
